ef54c8100248d42d6902862f19e03cd65d22089c
mwozniak97 <wozniakmichal777@gmail.com>
2023-02-12 18:35:08 +0100

    Init repo

---
+++ src/exerciseClass.java
@@ -0,0 +1,8 @@
+public class exerciseClass {
+
+    private String exerciseName;
+    private int caloriesBurned;
+
+    // TODO: Create another properties and methods
+
+}
---
+++ src/userClass.java
@@ -0,0 +1,8 @@
+public class userClass {
+
+    private String userHeight;
+    private String userWeight;
+
+    // TODO: Create another properties and methods
+
+}
